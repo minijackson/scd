4b827bbe4cd4f1fc7b845e1b1c112a1f9000b4db
Minijackson <minijackson@riseup.net>
2015-04-16 20:01:48 +0200

    Adding second version (real option parsing with horizontal and vertical split

--- init.zsh
+++ init.zsh
@@ -1,26 +1,61 @@
 function scd() {
-	if [[ -z "$1" || -z "$2" ]]; then
-		echo "Usage: $0 dir1 dir2"
-		return 64
-	else
-		dir1=$(realpath $1)
-		dir2=$(realpath $2)
+	local options
+	local dir1
+	local dir2
 
-		if [[ -d "$dir1" && -d "$dir2" ]]; then
-			if [[ -z "$TMUX" ]]; then
-				if [[ -z "$VIM" && -z "$EMACS" ]]; then
-					echo "To be implemented"
-					return 69
+	local stopOptionsParsing
+
+	for i in $* ; do
+		if [[ -z "$stopOptionsParsing" ]]; then
+			if [[ "$i" == "-v" || "$i" == "-h" ]]; then
+				if [[ -z "$options" ]]; then
+					options=$i
 				else
-					echo "Will not launch tmux in vim or emacs shell"
-					return 78
+					echo "Usage: $0 [-h|-v] [--] dir1 dir2"
+					return 64
 				fi
+			elif [[ "$i" == "--" ]]; then
+				stopOptionsParsing="Y"
+			elif [[ -z "$dir1" ]]; then
+				dir1=$(realpath -- $i)
+			elif [[ -z "$dir2" ]]; then
+				dir2=$(realpath -- $i)
 			else
-				cd $dir1
-				tmux split-window "zsh -c 'cd $dir2 ; zsh -i'"
+				echo "Usage: $0 [-h|-v] [--] dir1 dir2"
+				return 64
 			fi
 		else
-			echo "One of the dirs does not exist"
+			if [[ -z "$dir1" ]]; then
+				dir1=$(realpath -- $i)
+			elif [[ -z "$dir2" ]]; then
+				dir2=$(realpath -- $i)
+			else
+				echo "Usage: $0 [-h|-v] [--] dir1 dir2"
+				return 64
+			fi
 		fi
+	done
+
+	if [[ -z "$dir1" || -z "$dir2" ]]; then
+		echo "Usage: $0 [-h|-v] [--] dir1 dir2"
+		return 64
+	fi
+
+	if [[ -d "$dir1" && -d "$dir2" ]]; then
+		if [[ -z "$TMUX" ]]; then
+			if [[ -z "$VIM" && -z "$EMACS" ]]; then
+				echo "To be implemented"
+				return 69
+			else
+				echo "Will not launch tmux in vim or emacs shell"
+				return 78
+			fi
+		else
+			tmux split-window $options "zsh -c 'cd $dir2 ; zsh -i'"
+			cd $dir1
+			clear
+		fi
+	else
+		echo "One of the dirs does not exist"
 	fi
 }
